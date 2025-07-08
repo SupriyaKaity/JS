4ffb8428ab8c0cffb45807c0e8235b1fcab70fc1
Supriya Kaity <112101324+SupriyaKaity@users.noreply.github.com>
2025-07-08 14:48:52 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,27 +1,56 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN
 //true => 1; false => 0
 
+
+
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 =>false
 //"" => false
 //"Supriya" => true
 
 
+
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+//**********************Operatoions**********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "Supriya"
+let str2 = " Kaity"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log(+true); //1
+console.log(+""); //0
+
+
+
+
+
 
