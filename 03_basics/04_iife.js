09e28aaddd577047afb39c15e6aa0f2c121b0030
Supriya Kaity <112101324+SupriyaKaity@users.noreply.github.com>
2025-07-15 06:49:17 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})(); // need to close this function
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('supriya')
